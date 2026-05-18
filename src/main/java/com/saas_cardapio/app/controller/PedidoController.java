94fea6a7ccdf196fd8d18cb5772cbc844a36391a
joao <jovmamikl@gmail.com>
2026-05-18 11:37:53 -0300

    feat: implement PedidoController with criarPedido endpoint

--- src/main/java/com/saas_cardapio/app/controller/PedidoController.java
+++ src/main/java/com/saas_cardapio/app/controller/PedidoController.java
@@ -1,5 +1,31 @@
 package com.saas_cardapio.app.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.saas_cardapio.app.dto.PedidoRequest;
+import com.saas_cardapio.app.dto.PedidoResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@RestController
+@RequestMapping("/pedidos")
+@NoArgsConstructor
 public class PedidoController {
+
+    private final PedidoService pedidoService;
+
+
+    @PostMapping
+    public ResponseEntity<PedidoResponse> criarPedido(@RequestBody PedidoRequest pedidoRequest) {
+        PedidoResponse response = pedidoService.criarPedido(pedidoRequest);
+        return ResponseEntity.ok(response);
+    }
+
+
     
 }
