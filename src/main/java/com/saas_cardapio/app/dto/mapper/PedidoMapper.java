09cdb14bda80d47592365d15d9a8e190383c0e4e
joao <jovmamikl@gmail.com>
2026-05-18 14:27:24 -0300

    feat: implement PedidoMapper for mapping PedidoRequestDto and PedidoResponseDto

--- src/main/java/com/saas_cardapio/app/dto/mapper/PedidoMapper.java
+++ src/main/java/com/saas_cardapio/app/dto/mapper/PedidoMapper.java
@@ -1,5 +1,23 @@
 package com.saas_cardapio.app.dto.mapper;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import com.saas_cardapio.app.dto.PedidoRequestDto;
+import com.saas_cardapio.app.dto.PedidoResponseDto;
+import com.saas_cardapio.app.entity.Pedido;
+
+
+@Component
 public class PedidoMapper {
+
+    private final ModelMapper modelMapper = new ModelMapper();
     
+    public Pedido toPedido(PedidoRequestDto pedido){
+        return modelMapper.map(pedido, Pedido.class);
+    }
+
+    public PedidoResponseDto toPedidoResponse(Pedido pedido){
+        return modelMapper.map(pedido, PedidoResponseDto.class);
+    }
 }
