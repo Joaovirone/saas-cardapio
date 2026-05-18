e7fb492d9fa30cbee8ab10876aa4bc3afada6108
joao <jovmamikl@gmail.com>
2026-05-18 14:54:37 -0300

    fix: improve exception handling message in StreamLambdaHandler

--- src/main/java/com/saas_cardapio/app/handler/StreamLambdaHandler.java
+++ src/main/java/com/saas_cardapio/app/handler/StreamLambdaHandler.java
@@ -6,29 +6,27 @@ import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.saas_cardapio.app.AppApplication;
-
+import com.saas_cardapio.app.AppApplication; 
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class StreamLambdaHandler implements RequestStreamHandler{
+public class StreamLambdaHandler implements RequestStreamHandler {
     
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-
     static {
         try {
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(AppApplication.class);
         } catch (ContainerInitializationException e) {
-            // If we fail here. We re-throw the exception to force another cold start
             e.printStackTrace();
-            throw new RuntimeException("Não foi possível iniciar aplicação Spring Boot! :C", e);
+            throw new RuntimeException("Falha ao iniciar a aplicação Spring Boot na AWS", e);
         }
     }
 
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
         handler.proxyStream(inputStream, outputStream, context);
     }
 }
