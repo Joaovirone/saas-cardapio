9c93fc23fc2a66a9d6b18b26da9b3e4e62ed80da
joao <jovmamikl@gmail.com>
2026-05-18 14:25:05 -0300

    feat: update PedidoController to use DTOs and improve order processing response

--- src/main/java/com/saas_cardapio/app/controller/PedidoController.java
+++ src/main/java/com/saas_cardapio/app/controller/PedidoController.java
@@ -1,29 +1,31 @@
 package com.saas_cardapio.app.controller;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.saas_cardapio.app.dto.PedidoRequest;
-import com.saas_cardapio.app.dto.PedidoResponse;
+import com.saas_cardapio.app.dto.PedidoRequestDto;
+import com.saas_cardapio.app.dto.PedidoResponseDto;
+import com.saas_cardapio.app.service.PedidoService;
 
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+
 
 @RestController
 @RequestMapping("/pedidos")
-@NoArgsConstructor
+@AllArgsConstructor
 public class PedidoController {
 
     private final PedidoService pedidoService;
 
 
     @PostMapping
-    public ResponseEntity<PedidoResponse> criarPedido(@RequestBody PedidoRequest pedidoRequest) {
-        PedidoResponse response = pedidoService.criarPedido(pedidoRequest);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<PedidoResponseDto> criarPedido(@RequestBody PedidoRequestDto pedidoRequest) {
+        PedidoResponseDto response = pedidoService.processarPedido(pedidoRequest);
+        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(response);
     }
 
 
