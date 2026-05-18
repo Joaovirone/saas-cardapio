2b306af11443989e279441986a89de1a2f502ab0
joao <jovmamikl@gmail.com>
2026-05-18 14:24:37 -0300

    feat: add DynamoDb configuration for AWS integration

--- src/main/java/com/saas_cardapio/app/config/DynamoDbConfig.java
+++ src/main/java/com/saas_cardapio/app/config/DynamoDbConfig.java
@@ -1,5 +1,23 @@
 package com.saas_cardapio.app.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
 public class DynamoDbConfig {
     
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                    .region(Region.SA_EAST_1)
+                    .credentialsProvider(DefaultCredentialsProvider.create())
+                    .httpClient(UrlConnectionHttpClient.create())
+                    .build();
+    }
 }
