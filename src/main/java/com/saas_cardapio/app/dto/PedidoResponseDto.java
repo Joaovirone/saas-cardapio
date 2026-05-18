bdbe71c403b0722c8166e7f47f5bf466a3b008be
joao <jovmamikl@gmail.com>
2026-05-18 14:27:18 -0300

    feat: add PedidoResponseDto for order response handling

---
+++ src/main/java/com/saas_cardapio/app/dto/PedidoResponseDto.java
@@ -0,0 +1,13 @@
+package com.saas_cardapio.app.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class PedidoResponseDto{
+
+    String pedidoId;
+    String mensagem;
+    Double valorTotal;
+    String status;
+}
