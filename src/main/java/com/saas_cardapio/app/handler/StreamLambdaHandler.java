eb829da378fec395dc4d30ccbd2b2fbe8654b8cb
joao <jovmamikl@gmail.com>
2026-05-18 11:38:43 -0300

    feat: implement StreamLambdaHandler for AWS Lambda integration

--- src/main/java/com/saas_cardapio/app/handler/StreamLambdaHandler.java
+++ src/main/java/com/saas_cardapio/app/handler/StreamLambdaHandler.java
@@ -1,5 +1,34 @@
 package com.saas_cardapio.app.handler;
 
-public class StreamLambdaHandler {
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.saas_cardapio.app.AppApplication;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamLambdaHandler implements RequestStreamHandler{
     
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(AppApplication.class);
+        } catch (ContainerInitializationException e) {
+            // If we fail here. We re-throw the exception to force another cold start
+            e.printStackTrace();
+            throw new RuntimeException("Não foi possível iniciar aplicação Spring Boot! :C", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
 }
