7a67b93bc5f4bef31bf8a7a9f23c02c289951175
joao <jovmamikl@gmail.com>
2026-05-22 10:14:15 -0300

    feat: remove unused Lombok annotation from PedidoMapper

--- src/main/java/com/saas_cardapio/app/dto/mapper/PedidoMapper.java
+++ src/main/java/com/saas_cardapio/app/dto/mapper/PedidoMapper.java
@@ -7,9 +7,7 @@ import com.saas_cardapio.app.dto.PedidoRequestDto;
 import com.saas_cardapio.app.dto.PedidoResponseDto;
 import com.saas_cardapio.app.entity.Pedido;
 
-import lombok.NoArgsConstructor;
-
-
+@Component
 public class PedidoMapper {
 
     private final ModelMapper modelMapper;
