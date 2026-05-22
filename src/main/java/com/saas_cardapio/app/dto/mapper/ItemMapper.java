4e72dc1f284ad2c0f0eb54497632345e717ae26a
joao <jovmamikl@gmail.com>
2026-05-22 10:14:04 -0300

    feat: implement ItemMapper for mapping ItemRequestDto to ItemPedido entity

--- src/main/java/com/saas_cardapio/app/dto/mapper/ItemMapper.java
+++ src/main/java/com/saas_cardapio/app/dto/mapper/ItemMapper.java
@@ -1,8 +1,21 @@
 package com.saas_cardapio.app.dto.mapper;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import com.saas_cardapio.app.dto.ItemRequestDto;
+import com.saas_cardapio.app.entity.ItemPedido;
 
 @Component
 public class ItemMapper {
     
+        private final ModelMapper modelMapper;
+
+    public ItemMapper(){
+        this.modelMapper = new ModelMapper();
+    }
+    
+    public ItemPedido toItemPedido(ItemRequestDto pedido){
+        return modelMapper.map(pedido, ItemPedido.class);
+    }
 }
