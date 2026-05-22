fb22bfaaa4d555e3fe92be2d4e227e4853709888
joao <jovmamikl@gmail.com>
2026-05-22 10:13:35 -0300

    feat: implement RegistroMapper for mapping RegistroRequestDto to Cliente entity

--- src/main/java/com/saas_cardapio/app/dto/mapper/RegistroMapper.java
+++ src/main/java/com/saas_cardapio/app/dto/mapper/RegistroMapper.java
@@ -1,8 +1,22 @@
 package com.saas_cardapio.app.dto.mapper;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import com.saas_cardapio.app.dto.RegistroRequestDto;
+import com.saas_cardapio.app.entity.Cliente;
+
 
 @Component
 public class RegistroMapper {
     
+        private final ModelMapper modelMapper;
+
+    public RegistroMapper(){
+        this.modelMapper = new ModelMapper();
+    }
+    
+    public Cliente toCliente(RegistroRequestDto pedido){
+        return modelMapper.map(pedido, Cliente.class);
+    }
 }
